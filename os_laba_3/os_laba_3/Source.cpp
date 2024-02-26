3fab7047e20f12888ff60fb8511fd9e0fd865e70
listussr <116896338+listussr@users.noreply.github.com>
2024-02-26 18:34:14 +0400

    Update Source.cpp

--- os_laba_3/os_laba_3/Source.cpp
+++ os_laba_3/os_laba_3/Source.cpp
@@ -25,83 +25,34 @@ typedef long double ld;
     return _factorial;
 }*/
 
-int fd_in[2];
-int fd_out[2];
-pid_t pid;
+/// @brief namespace with the global variables
+namespace lab
+{
+    int fd_in [2];
+    int fd_out[2];
+    pid_t pid;
+    bool file_flag = false;
+    const char* input_filename;
+    const char* output_filename;
+}
 
-void show_help_message() {
+/// @brief function that shows an informational message
+void show_help_message()
+{
     cout << "To count current function with data from console you should restart program without any keys" << '\n';
     cout << "If you want to compute function with file input and output restart programm with the keys --file [INPUT FILENAME] [OUTPUT FILENAME]" << '\n';
 }
 
-ld enter(const char* msg) {
-    cout << msg << '\t';
-    std::string str_input;
-    ld input;
-    bool error_flag = true;
-    while (error_flag) {
-        cin >> str_input;
-        try {
-            input = std::stold(str_input);
-            error_flag = false;
-        }
-        catch (std::invalid_argument) {
-            cout << "Invalid argument! Retry input" << '\n';
-        }
-    }
-    return input;
-}
 
-ld function(ld& x) {
-    ld sum = 0;
-    int factorial = 1;
-    for (int n = 0; n <= 100; ++n) {
-        sum += (sin(x) + cos(n)) / (x + factorial);
-        factorial *= (n > 0) ? n : 1;
-    }
-    return sum;
-}
-
-void server() {
-    ld x;
-    read(fd_in[0], &x, sizeof(ld));
-    ld result = function(x);
-    write(fd_out[1], &result, sizeof(ld));
-}
-
-void client() {
-    ld x = enter("Enter x. X is the number with a floating point");
-    write(fd_in[1], &x, sizeof(ld));
-    ld result;
-    read(fd_out[0], &result, sizeof(ld));
-    cout << "f(x) = " << result << '\n';
-}
-
-void console_input() {
-    cout << "Program for calculating function f(x) = sum(n = {0-100}) (sin(x) + cos(n)) / (x + n!)" << '\n';
-    pipe(fd_in);
-    pipe(fd_out);
-    pid = fork();
-    if (pid < 0) {
-        std::cerr << "Failure in the forking!" << '\n';
-        exit(1);
-    }
-    else if (pid > 0) {
-        client();
-    }
-    else {
-        server();
-    }
-    for (int i = 0; i < 2; ++i) {
-        close(fd_in[i]);
-        close(fd_out[i]);
-    }
-}
-
-ld scan(const char* filename) {
+/// @brief function that takes the value from the file
+/// @param filename 
+/// @return value -> long double
+ld scan(const char* filename)
+{
     ifstream in;
     in.open(filename, std::ios::binary);
-    if (!in.is_open()) {
+    if(!in.is_open())
+    {
         std::cerr << "Incorrect input filename!" << '\n';
         exit(1);
     }
@@ -110,67 +61,150 @@ ld scan(const char* filename) {
     ld data;
     in >> str_data;
     in.close();
-    try {
+    try
+    {
         data = std::stold(str_data);
     }
-    catch (std::invalid_argument) {
+    catch(std::invalid_argument)
+    {
         std::cerr << "Incorrect data in the input file!" << '\n';
         exit(1);
     }
     return data;
 }
 
-void load(const char* filename, ld& value) {
+/// @brief function of loading value to the output file
+/// @param filename 
+/// @param value 
+void load(const char* filename, ld& value)
+{
     ofstream out;
     out.open(filename, std::ios::binary);
     out << value;
     out.close();
 }
 
-void file_client(const char* in_filename, const char* out_filename) {
-    ld x = scan(in_filename);
-    write(fd_in[1], &x, sizeof(ld));
-    ld result;
-    read(fd_out[0], &result, sizeof(ld));
-    cout << "Check the output file" << '\n';
-    load(out_filename, result);
+/// @brief function of getting a console input
+/// @param msg 
+/// @return value -> long double
+ld enter(const char* msg)
+{
+    cout << msg << '\t';
+    std::string str_input;
+    ld input;
+    bool error_flag = true;
+    // waiting until we do not get a correct value
+    while(error_flag)
+    {
+        cin >> str_input;
+        try
+        {
+            input = std::stold(str_input);
+            error_flag = false;
+        }
+        catch(std::invalid_argument)
+        {
+            cout << "Invalid argument! Retry input" << '\n';
+        }
+    }
+    return input;
 }
 
-void file_input(const char* in_filename, const char* out_filename) {
+/// @brief function that counts Sum from the task
+/// @param x 
+/// @return result -> long double
+ld function(ld& x)
+{
+    ld sum = 0;
+    int factorial = 1;
+    for(int n = 0; n <= 100; ++n)
+    {
+        sum += (sin(x) + cos(n)) / (x + factorial);
+        factorial *= (n > 0)? n: 1;
+    }
+    return sum;
+}
+
+/// @brief function with the "backend" side of the program
+void server()
+{
+    ld x;
+    read(lab::fd_in[0], &x, sizeof(ld));
+    ld result = function(x);
+    write(lab::fd_out[1], &result, sizeof(ld));
+}
+
+/// @brief function with the "frontend" side of the program
+void client()
+{
+    ld x;
+    if(lab::file_flag)
+    {
+        x = scan(lab::input_filename);
+    } 
+    else
+    {
+        x = enter("Enter x. X is the number with a floating point");
+    }
+    write(lab::fd_in[1], &x, sizeof(ld));
+    ld result;
+    read(lab::fd_out[0], &result, sizeof(ld));
+    if(lab::file_flag)
+    {
+        cout << "Check the output file" << '\n';
+        load(lab::output_filename, result);
+    }
+    else
+    {
+        cout << "f(x) = " << result << '\n';
+    }
+}
+
+/// @brief main processing function of the program
+void process()
+{
     cout << "Program for calculating function f(x) = sum(n = {0-100}) (sin(x) + cos(n)) / (x + n!)" << '\n';
-    pipe(fd_in);
-    pipe(fd_out);
-    pid = fork();
-    if (pid < 0) {
+    pipe(lab::fd_in);
+    pipe(lab::fd_out);
+    lab::pid = fork();
+    if(lab::pid < 0)
+    {
         std::cerr << "Failure in the forking!" << '\n';
         exit(1);
     }
-    else if (pid > 0) {
-        file_client(in_filename, out_filename);
+    else if(lab::pid > 0)
+    {
+        client();
     }
-    else {
+    else
+    {
         server();
     }
-    for (int i = 0; i < 2; ++i) {
-        close(fd_in[i]);
-        close(fd_out[i]);
+    for(int i = 0; i < 2; ++i)
+    {
+        close(lab::fd_in[i]);
+        close(lab::fd_out[i]);
     }
 }
 
-int main(int argc, char* argv[]) {
-    if (argc == 2 && !strcmp(argv[1], "--help")) {
+int main(int argc, char* argv[])
+{
+    if(argc == 2 && !strcmp(argv[1], "--help"))
+    {
         show_help_message();
-        exit(0);
+        return 0;
     }
-    else if (argc == 4 && !strcmp(argv[1], "--file")) {
-        file_input(argv[2], argv[3]);
+    else if(argc == 4 && !strcmp(argv[1], "--file"))
+    {
+        lab::file_flag = true;
+        lab::input_filename = argv[2];
+        lab::output_filename = argv[3];
     }
-    else if (argc > 2 && argc != 4) {
+    else if(argc > 2 && argc != 4)
+    {
         cout << "Restart program with the key [--help]" << '\n';
-        exit(1);
+        return 1;
     }
-    else {
-        console_input();
-    }
+    process();
     return 0;
 }
